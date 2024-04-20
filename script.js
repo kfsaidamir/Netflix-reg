c3104e4ac50391a6eaaf96f0fe4f5293424344ed
kf_saidamir <skadirov2009@gmail.com>
2024-04-20 21:28:30 +0500

    a

--- script.js
+++ script.js
@@ -3,23 +3,26 @@
 // Valigatsiya
 //https://regex101.com/
 
-const regExName =  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+// const regExName =  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const regExName = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g
 const regExNumber = /^[0-9]{9,12}$/
+
 const text = document.querySelector('.text')
 const placeholder2 = document.querySelector('.password::-webkit-input-placeholder')
 const password = document.querySelector('.password')
-const form = document.querySelector('form')
+const form = document.querySelector('form > button ')
 const dark = document.querySelector('.dark')
 const reg = document.querySelector('.regestration')
 const h2 = document.querySelector('h2')
 const h1 = document.querySelector('h1')
 const section = document.querySelector('section')
 const but = document.querySelector('.but')
-const email = "example@example.com";
+// const email = "example@example.com";
 const kil = document.querySelector('.kil')
 const lit = document.querySelector('.lit')
+
 text.addEventListener('keyup', () => {
-    if (regExName.test(email)) {
+    if (regExName.test(text.value)) {
         text.style.border = '2px solid green'
     } else {
         text.style.border = '2px solid red'
@@ -34,7 +37,7 @@ password.addEventListener('keyup', () => {
     }
 });
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('click', (e) => {
     e.preventDefault()
     if (regExName.test(text.value) && regExNumber.test(password.value)) {
         document.location = 'netflix.html'
